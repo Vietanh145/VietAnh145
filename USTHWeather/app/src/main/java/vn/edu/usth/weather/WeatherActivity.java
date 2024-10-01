ada33bbba524eb82d02c6bf9201a1570811027d6
Vietanh145 <nguyenvietanh9955@gmail.com>
2024-10-01 22:08:19 +0700

    Update practical work 11 and 12

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,28 +1,31 @@
 package vn.edu.usth.weather;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
-
-import androidx.activity.EdgeToEdge;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import android.util.*;
-import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
-import com.google.android.material.tabs.TabLayout;
-import java.util.ArrayList;
-import java.util.List;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import vn.edu.usth.weather.databinding.ActivityWeatherBinding;
 
 
 public class WeatherActivity extends AppCompatActivity {
 
-//    private static final String TAG = "MainActivity";
+    private static final String TAG = "WeatherActivity";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -40,8 +43,16 @@ public class WeatherActivity extends AppCompatActivity {
         ActivityWeatherBinding binding = ActivityWeatherBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+
         ViewPager viewPager = binding.viewPager;
         TabLayout tabLayout = binding.tabLayout;
+
+        // Extract the MP3 file to external storage
+        extractMP3ToExternalStorage();
+        // Play the MP3 after extraction
+        playMP3FromExternalStorage();
 
         WeatherPagerAdapter adapter = new WeatherPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
@@ -80,6 +91,57 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i("Weather", "onDestroy Call");
     }
-
+    private void extractMP3ToExternalStorage() {
+        try {
+            InputStream inputStream = getResources().openRawResource(R.raw.file);
+            File outFile = new File(getExternalFilesDir(null), "file.mp3");
+            FileOutputStream outputStream = new FileOutputStream(outFile);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            outputStream.close();
+            inputStream.close();
+            Log.i(TAG, "MP3 extracted to: " + outFile.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e(TAG, "Error extracting MP3", e);
+        }
+    }
+    private void playMP3FromExternalStorage() {
+        try {
+            File mp3File = new File(getExternalFilesDir(null), "file.mp3");
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(mp3File.getAbsolutePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            Log.i(TAG, "Playing MP3");
+        } catch (IOException e) {
+            Log.e(TAG, "Error playing MP3", e);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_weather.xml
+        getMenuInflater().inflate(R.menu.menu_weather, menu);
+        return true;
+    }
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                // Show a toast when the refresh action is clicked
+                Toast.makeText(this, "Refreshing...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                // Start PrefActivity when the settings action is clicked
+                Intent intent = new Intent(this, PrefActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
